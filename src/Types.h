4a759ad5c14de763bf56d1f98dae7ad34831ad25
littleAmerica <dimitry.poberezhny@gmail.com>
2015-06-30 00:45:13 +0300

    global refactoring in progressing

--- src/Types.h
+++ src/Types.h
@@ -2,19 +2,44 @@
 #define __TYPES__
 
 
-struct Point
+struct Point_t
 {
-	Point(int x_, int y_):x(x_),y(y_){}
-	int x;
-	int y;
+	float x;
+	float y;
+	
+	Point_t():
+		x(0.f),
+		y(0.f)
+	{
+	}
+	Point_t(float _x, float _y):
+		x(_x),
+		y(_y)
+	{
+	}
 };
 
-struct Rect
+struct Rect_t
 {
-	int x1;
-	int x2;
-	int y1;
-	int y2;
+	float x1;
+	float x2;
+	float y1;
+	float y2;
+	
+	Rect_t():
+		x1(0.f),
+		x2(0.f),
+		y1(0.f),
+		y2(0.f)
+	{
+	}
+	Rect_t(float _x1, float _x2, float _y1, float _y2):
+		x1(_x1),
+		x2(_x2),
+		y1(_y1),
+		y2(_y2)
+	{
+	}
 };
 
 #endif
